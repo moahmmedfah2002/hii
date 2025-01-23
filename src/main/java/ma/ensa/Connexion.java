f19ec72b300dfb08b35fb757962cafa5900e1950
pc <fahlaouimohammed@gmail.com>
2025-01-23 19:12:09 +0100

    test

--- src/main/java/ma/ensa/Connexion.java
+++ src/main/java/ma/ensa/Connexion.java
@@ -10,5 +10,6 @@ public class Connexion {
         Class.forName("com.mysql.cj.jdbc.Driver");
         con= DriverManager.getConnection("jdbc:mysql://mysql-3a17bb18-fahlaouimohammed-0d14.a.aivencloud.com:18736/cl","avnadmin","AVNS_-kGkj9fjvYBtons8NWE");
         System.out.println("Connexion established");
+        System.out.println("hi");
     }
 }
