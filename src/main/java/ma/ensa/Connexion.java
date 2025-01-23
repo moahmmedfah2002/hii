e3c800aa9ea285263e3fefb58549bc1a026a16f1
pc <fahlaouimohammed@gmail.com>
2025-01-23 18:25:38 +0100

    test

---
+++ src/main/java/ma/ensa/Connexion.java
@@ -0,0 +1,14 @@
+package ma.ensa;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connexion {
+    private Connection con ;
+    public Connexion() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con= DriverManager.getConnection("jdbc:mysql://mysql-3a17bb18-fahlaouimohammed-0d14.a.aivencloud.com:18736/cl","avnadmin","AVNS_-kGkj9fjvYBtons8NWE");
+        System.out.println("Connexion established");
+    }
+}
---
+++ src/main/java/ma/ensa/Main.java
@@ -0,0 +1,32 @@
+package ma.ensa;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Client client1 = new Client(2,"mohammed",23);
+        Client client2 = new Client(2,"mohammed",30);
+        Client client3 = new Client(2,"mohammed",50);
+        List<Client> clients = new ArrayList<>();
+        clients.add(client2);
+        clients.add(client1);
+        clients.add(client3);
+
+        clients.sort(Comparator.comparingInt(Client::getAge));
+        clients.forEach(System.out::println);
+
+
+        Connexion connexion=new Connexion();
+//        for (Client client : clients) {
+//            System.out.println(client);
+//        }
+
+
+
+    }
+}
